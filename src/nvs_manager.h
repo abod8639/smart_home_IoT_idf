2348735a46e2baa57374282665bf75ec18d43f6c
abot8639 <abot8639@gmail.com>
2026-06-05 07:18:34 +0300

    feat: add target temperature persistence, enable HTTPD WebSocket support, and implement PWM duty retrieval

--- src/nvs_manager.h
+++ src/nvs_manager.h
@@ -6,5 +6,7 @@
 void nvs_manager_init(void);
 void nvs_save_pin_state(uint8_t pin, int state);
 int nvs_get_pin_state(uint8_t pin, int default_val);
+void nvs_save_target_temp(int temp);
+int nvs_get_target_temp(int default_val);
 
 #endif // NVS_MANAGER_H
