3d3e2094629515e2f0ca6267b035d120494d861b
abot8639 <abot8639@gmail.com>
2026-06-05 23:50:44 +0300

    feat: initialize Google Home action cloud function and dependencies

--- src/pwm_manager.h
+++ src/pwm_manager.h
@@ -2,11 +2,7 @@
 #define PWM_MANAGER_H
 
 #include <stdint.h>
-
-#define PWM_LAMP_PIN 22
-#define PWM_RGB_R_PIN 23
-#define PWM_RGB_G_PIN 25
-#define PWM_RGB_B_PIN 26
+#include "device_config.h"   // Pin definitions (PWM_LAMP_PIN, PWM_RGB_*)
 
 void pwm_manager_init(void);
 void pwm_set_duty(uint8_t pin, uint32_t duty);
