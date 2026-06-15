8ac6a3f3ca0a7124f9f3ce41426dcc85854e02c0
abot8639 <abot8639@gmail.com>
2026-06-15 04:15:33 +0300

    feat: implement persistent WiFi credential storage in NVS and add captive portal provisioning mode

--- src/nvs_manager.h
+++ src/nvs_manager.h
@@ -2,11 +2,15 @@
 #define NVS_MANAGER_H
 
 #include <stdint.h>
-
+#include <stdbool.h>
+#include <stddef.h>
 void nvs_manager_init(void);
 void nvs_save_pin_state(uint8_t pin, int state);
 int nvs_get_pin_state(uint8_t pin, int default_val);
 void nvs_save_target_temp(int temp);
 int nvs_get_target_temp(int default_val);
+
+void nvs_save_wifi_credentials(const char* ssid, const char* password);
+bool nvs_get_wifi_credentials(char* ssid, size_t ssid_len, char* password, size_t password_len);
 
 #endif // NVS_MANAGER_H
