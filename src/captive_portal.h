2998222ac997efba8456795ea0efbd73042a632b
abot8639 <abot8639@gmail.com>
2026-06-15 04:15:18 +0300

    feat: implement captive portal for Wi-Fi provisioning and persistent credential storage in NVS

---
+++ src/captive_portal.h
@@ -0,0 +1,10 @@
+#ifndef CAPTIVE_PORTAL_H
+#define CAPTIVE_PORTAL_H
+
+/**
+ * @brief Starts the HTTP web server and the simple UDP DNS server
+ *        required for the Captive Portal experience.
+ */
+void captive_portal_start(void);
+
+#endif // CAPTIVE_PORTAL_H
