8ce0f3a4b7d8ccada24b04ab4eab35aeca07540b
abot8639 <abot8639@gmail.com>
2026-06-27 04:05:00 +0300

    feat: implement Matter endpoint management and integration, and disable DHT22 sensor monitoring task

--- src/dht_sensor.h
+++ src/dht_sensor.h
@@ -1,16 +1,16 @@
-#ifndef DHT_SENSOR_H
-#define DHT_SENSOR_H
+// #ifndef DHT_SENSOR_H
+// #define DHT_SENSOR_H
 
-#include "esp_err.h"
-#include "device_config.h"   // Pin definitions (DHT_PIN)
+// #include "esp_err.h"
+// #include "device_config.h"   // Pin definitions (DHT_PIN)
 
-// Initialize DHT
-void dht_sensor_init(void);
+// // Initialize DHT
+// void dht_sensor_init(void);
 
-// Read DHT22
-esp_err_t dht_sensor_read(float *temperature, float *humidity);
+// // Read DHT22
+// esp_err_t dht_sensor_read(float *temperature, float *humidity);
 
-float dht_sensor_get_temperature(void);
-float dht_sensor_get_humidity(void);
+// float dht_sensor_get_temperature(void);
+// float dht_sensor_get_humidity(void);
 
-#endif // DHT_SENSOR_H
+// #endif // DHT_SENSOR_H
